5da3bf877e4317ebae8ccfbf7d7ba130ccaf26cc
ngarng00 <ngarng00@estudiantes.unileon.es>
2025-09-16 14:56:18 +0200

    feat: componentes clase, ciclo d vida y metodos

--- frontend/src/componentes/ComponenteClase2.tsx
+++ frontend/src/componentes/ComponenteClase2.tsx
@@ -18,11 +18,34 @@ export class ComponenteClase2 extends React.Component<ComponenteProps, Component
     render() {
         console.log("render");
         return <>
-            Hola, this is {this.state.name} from a ComponenteClase2
+            Hola, this is {this.state.name} from a ComponenteClase2<br />
+            <button onClick={()=>{
+                this.setState({name: "Kike"})
+            }}>
+                Cambiar nombre
+            </button>
         </>
     }
 
+    componentDidMount() {
+        console.log("componentDidMount");
+    }
 
+    componentDidUpdate(prevProps: Readonly<ComponenteProps>, prevState: Readonly<ComponenteState>, snapshot?: any): void {
+        console.log("componentDidUpdate");
+        console.log("Prev props");
+        console.log(JSON.stringify(prevProps));
+        console.log("current props");
+        console.log(JSON.stringify(this.props));
+        console.log("Prev state");
+        console.log(JSON.stringify(prevState));
+        console.log("current state");
+        console.log(JSON.stringify(this.state));
+    }
+
+    componentWillUnmount(): void {
+        console.log("componentWillUnmount");
+    }
 }
 
 interface ComponenteState {
